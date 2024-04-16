01114346379e02783822d0552bd764f4f7fc309c
Agaltsev Stas <agaltsev.stas@mail.ru>
2024-04-16 17:42:03 +0300

    Плотная упаковка полей

--- OOP/Aligment.cpp
+++ OOP/Aligment.cpp
@@ -80,7 +80,7 @@ namespace aligment
                 [[maybe_unused]] auto c3_offset = offsetof(Padding, c3); // offset: 10
                 
                 /*
-                 Хранение Padding в памяти:
+                 Хранение Padding в блоках памяти по 4 байта:
                  4      |4      |1,1,1
                  0,1,2,3,4,5,6,7,8,9,10,11
                  */
@@ -107,7 +107,7 @@ namespace aligment
                 [[maybe_unused]] auto number2_offset = offsetof(Padding, number2); // offset: 8
                 
                 /*
-                 Хранение Padding в памяти:
+                 Хранение Padding в блоках памяти по 4 байта:
                  1,1,1,1|4      |4
                  0,1,2,3,4,5,6,7,8,9,10,11
                  */
@@ -130,46 +130,134 @@ namespace aligment
                 [[maybe_unused]] auto c2_offset = offsetof(Padding, c2); // offset: 16
                 [[maybe_unused]] auto c3_offset = offsetof(Padding, c3); // offset: 17
                 [[maybe_unused]] auto number_offset = offsetof(Padding, number); // offset: 20
+                
+                /*
+                 Хранение Padding в блоках памяти по 4 байта:
+                 1              |8                    |1, 1       |4
+                 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23
+                 */
             }
             // 6 Способ: выравнивание при наследовании == выравнивание сложных объектов
             {
-                struct Base // Должно быть bytes: 5
+                // Раннее связывание - статический полиморфизм
                 {
-                    int number; // bytes: 4
-                    char c;     // bytes: 1
-                };
-                
-                [[maybe_unused]] auto base_padding_size = sizeof(Base); // На самом деле bytes: 8 - размер структуры
-                [[maybe_unused]] auto base_padding_align = alignof(Base); // bytes: 4 - выравнивание по границе
-                [[maybe_unused]] auto base_number_offset = offsetof(Base, number); // offset: 0
-                [[maybe_unused]] auto base_c_offset = offsetof(Base, c); // // offset: 4
-                
-                // Выравнивание при наследовании
-                struct Derived : Base // Должно быть bytes: 6
+                    struct Base // Должно быть bytes: 5
+                    {
+                        int number; // bytes: 4
+                        char c;     // bytes: 1
+                    };
+                    
+                    [[maybe_unused]] auto base_padding_size = sizeof(Base); // На самом деле bytes: 8 - размер структуры
+                    [[maybe_unused]] auto base_padding_align = alignof(Base); // bytes: 4 - выравнивание по границе
+                    [[maybe_unused]] auto base_number_offset = offsetof(Base, number); // offset: 0
+                    [[maybe_unused]] auto base_c_offset = offsetof(Base, c); // // offset: 4
+                    
+                    /*
+                     Хранение Padding в блоках памяти по 4 байта:
+                     4      |1
+                     0,1,2,3,4,5,6,7
+                     */
+                    
+                    // Выравнивание при наследовании
+                    struct Derived : Base // Должно быть bytes: 6
+                    {
+                        char c; // bytes: 1
+                    };
+                    
+                    [[maybe_unused]] auto derived_padding_size = sizeof(Derived); // На самом деле bytes: 8 - размер структуры
+                    [[maybe_unused]] auto derived_padding_align = alignof(Derived); // bytes: 4 - выравнивание по границе
+                    [[maybe_unused]] auto derived_c_offset = offsetof(Derived, c); // offset: 8
+                    
+                    /*
+                     Хранение Padding в блоках памяти по 4 байта:
+                     4      |4      |1
+                     0,1,2,3,4,5,6,7,8,9,10,11
+                     */
+                    
+                    // Выравнивание сложных объектов
+                    struct Example // Должно быть bytes: 11
+                    {
+                        Base base; // bytes: 8
+                        char c;     // bytes: 1
+                    };
+                    
+                    [[maybe_unused]] auto example_padding_size = sizeof(Example); // На самом деле bytes: 12 - размер структуры
+                    [[maybe_unused]] auto example_padding_align = alignof(Example); // bytes: 4 - выравнивание по границе
+                    [[maybe_unused]] auto example_base_offset = offsetof(Example, base); // offset: 0
+                    [[maybe_unused]] auto example_c_offset = offsetof(Example, c); // offset: 8
+     
+                    [[maybe_unused]] auto compare = sizeof(Derived) == sizeof(Example);
+                    
+                    /*
+                     Хранение Padding в блоках памяти по 4 байта:
+                     4      |4      |1
+                     0,1,2,3,4,5,6,7,8,9,10,11
+                     */
+                }
+                // Позднее связывание - динамический полиморфизм,
                 {
-                    char c; // bytes: 1
-                };
-                
-                [[maybe_unused]] auto derived_padding_size = sizeof(Derived); // На самом деле bytes: 8 - размер структуры
-                [[maybe_unused]] auto derived_padding_align = alignof(Derived); // bytes: 4 - выравнивание по границе
-                [[maybe_unused]] auto derived_c_offset = offsetof(Derived, c); // offset: 8
-                
-                // Выравнивание сложных объектов
-                struct Example // Должно быть bytes: 11
-                {
-                    Base base; // bytes: 8
-                    char c;     // bytes: 1
-                };
-                
-                [[maybe_unused]] auto example_padding_size = sizeof(Example); // На самом деле bytes: 20 - размер структуры
-                [[maybe_unused]] auto example_padding_align = alignof(Example); // bytes: 4 - выравнивание по границе
-                [[maybe_unused]] auto example_base_offset = offsetof(Example, base); // offset: 0
-                [[maybe_unused]] auto example_c_offset = offsetof(Example, c); // offset: 8
- 
-                [[maybe_unused]] auto compare = sizeof(Derived) == sizeof(Example);
+                    struct Base // Должно быть bytes: 4
+                    {
+                        virtual void function() = 0;
+                        virtual ~Base() = default;
+                        int number; // bytes: 4
+                    };
+                    
+                    [[maybe_unused]] auto base_padding_size = sizeof(Base); // На самом деле bytes: 16 - размер структуры
+                    [[maybe_unused]] auto base_padding_align = alignof(Base); // bytes: 8 - выравнивание по границе
+                    [[maybe_unused]] auto base_number_offset = offsetof(Base, number); // offset: 8
+                    
+                    /*
+                     Хранение Padding в блоках памяти по 8 байтов:
+                     8              |4
+                     0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15
+                     */
+                    
+                    // Выравнивание при наследовании
+                    struct Derived1 : Base // Должно быть bytes: 5
+                    {
+                        char c; // bytes: 1
+                        void function() override {};
+                    };
+                    
+                    // Выравнивание при наследовании
+                    struct Derived2 : Base // Должно быть bytes: 5
+                    {
+                        char c; // bytes: 1
+                        void function() override {};
+                    };
+                    
+                    [[maybe_unused]] auto derived_padding_size = sizeof(Derived1); // На самом деле bytes: 16 - размер структуры
+                    [[maybe_unused]] auto derived_padding_align = alignof(Derived1); // bytes: 8 - выравнивание по границе
+                    [[maybe_unused]] auto derived_c_offset = offsetof(Derived1, c); // offset: 12
+                    
+                    /*
+                     Хранение Padding в блоках памяти по 8 байтов:
+                     8              |4        |1
+                     0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15
+                     */
+                    
+                    // Приводит первые 8 байтов к указателю vpointer
+                    auto PrintVPointer = [](void* object) -> void
+                    {
+                        auto pointer = reinterpret_cast<size_t*>(object);
+                        std::cout << "VPointer: " << *pointer << std::endl;
+                    };
+                    
+                    Derived1 derived11, derived12;
+                    
+                    std::cout << "VPointer один для экземпляров класса Derived1: " << std::endl;
+                    PrintVPointer(&derived11);
+                    PrintVPointer(&derived12);
+                    
+                    std::cout << "VPointer один для экземпляров класса Derived2: " << std::endl;
+                    Derived2 derived21, derived22;
+                    PrintVPointer(&derived21);
+                    PrintVPointer(&derived22);
+                }
             }
             /*
-             Современные процессоры не загружают данные по байту из памяти, обычно по 4 байта сразу: если требуемые данные не выравнены по 4 байтам, то процессору приходится производить разбиение для записи в кэш, а при чтении объединять требуемые байты, что приводит к уменьшению производительности ЦП при чтении до 3-х раз.
+             7 Способ: Современные процессоры не загружают данные по байту из памяти, обычно по 4 байта сразу: если требуемые данные не выравнены по 4 байтам, то процессору приходится производить разбиение для записи в кэш, а при чтении объединять требуемые байты, что приводит к уменьшению производительности ЦП при чтении до 3-х раз.
              C++17: спецификатор alignas - позволяющий вручную указать компилятору границу выравнивание для типа или переменой (2,4,8,16,32 байта) - это помогает уменьшить число разбиений данных для хранения кэшах и объединений при чтении данных ЦП.
              Плюсы:
              - увеличение производительности за счет сокращения циклов ЦП при чтении.
@@ -196,11 +284,101 @@ namespace aligment
                 [[maybe_unused]] auto c3_offset = offsetof(Padding, c3); // offset: 6
                 [[maybe_unused]] auto number2_offset = offsetof(Padding, number2); // offset: 8
                 [[maybe_unused]] auto number3_offset = offsetof(Padding, number3); // offset: 12
+                
                 /*
-                 Хранение Padding в блоках памяти по 4 байта:
-                 4      |1,1,1  |4        |4
+                 Хранение Padding в блоках памяти по 16 байт:
+                 4       1,1,1   4         4          |
                  0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15
                  */
+            }
+            /*
+             8 Способ: отключение выравнивания с помощью макроса #pragma pack (push, 1), 1 - устанавливает размер выравнивания в байтах.
+             Плюсы: уменьшение потребляемой памяти.
+             Минусы: уменьшение скорости работы.
+             */
+            {
+#pragma pack (push, 1)
+                struct Padding // Должно быть bytes: 15
+                {
+                    char c1;     // bytes: 1
+                    double flag; // bytes: 8
+                    char c2;     // bytes: 1
+                    char c3;     // bytes: 1
+                    int number;  // bytes: 4
+                };
+#pragma pack (pop)
+                
+                [[maybe_unused]] auto padding_size = sizeof(Padding); // На самом деле bytes: 15 - размер структуры
+                [[maybe_unused]] auto padding_align = alignof(Padding); // bytes: 1 - выравнивание по границе
+                [[maybe_unused]] auto c1_offset = offsetof(Padding, c1); // bytes: 0 - выравнивание по границе
+                [[maybe_unused]] auto flag_offset = offsetof(Padding, flag); // offset: 1
+                [[maybe_unused]] auto c2_offset = offsetof(Padding, c2); // offset: 9
+                [[maybe_unused]] auto c3_offset = offsetof(Padding, c3); // offset: 10
+                [[maybe_unused]] auto number_offset = offsetof(Padding, number); // offset: 11
+                
+                /*
+                 Хранение Padding в блоках памяти по 1 байту:
+                 1|8              |1|1 |4
+                 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15
+                 */
+            }
+            /*
+             Плотная упаковка полей - опуститься до уровней битов.
+             Плюсы: уменьшение потребляемой памяти.
+             Минусы: уменьшение скорости работы.
+             */
+            {
+                // 1 Способ: битовые поля
+                {
+                    struct Padding // Должно быть bytes: 4
+                    {
+                        uint16_t number1:10; // bits: 10
+                        uint8_t c1:4;        // bits: 4
+                        uint8_t c2:4;        // bits: 4
+                        uint8_t c3:4;        // bits: 4
+                        uint16_t number2:10; // bits: 10
+                    };
+                    
+                    [[maybe_unused]] auto padding_size = sizeof(Padding); // На самом деле bytes: 6 - размер структуры
+                    [[maybe_unused]] auto padding_align = alignof(Padding); // bytes: 2 - выравнивание по границе
+                    
+                    /*
+                     c1 + c2 уйдут в 1 бит, с3 займет 0,5 бита -> c1 + c2 + c3 = 1,5 байта
+                     Хранение Padding в блоках памяти по 2 байта:
+                     2  |1,5|2
+                     0,1,2,3,4,5
+                     */
+                }
+                // 2 Способ: подогнать размер все полей структуры в один тип
+                {
+                    struct Padding // Должно быть bytes: 4
+                    {
+                        uint32_t number1:10; // bits: 10
+                        uint32_t c1:4;       // bits: 4
+                        uint32_t c2:4;       // bits: 4
+                        uint32_t c3:4;       // bits: 4
+                        uint32_t number2:10; // bits: 10
+                    };
+                    
+                    [[maybe_unused]] auto padding_size = sizeof(Padding); // На самом деле bytes: 4 - размер структуры
+                    [[maybe_unused]] auto padding_align = alignof(Padding); // bytes: 2 - выравнивание по границе
+                    
+                    static_assert(sizeof(Padding) == 4, "Wrong message!");
+    
+                    /*
+                     number1 + c1 + c2 + c3 + number2 = 4 байта
+                     Хранение Padding в блоках памяти по 4 байта:
+                     4
+                     0,1,2,3
+                     */
+                    
+                    /*
+                     number1 + c1 + c2 + c3 + number2 = 32 бита
+                     Хранение Padding в блоках памяти по 32 байта:
+                     number1            |c1         |c2         |c3         |number2
+                     0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32
+                     */
+                }
             }
         }
     }
